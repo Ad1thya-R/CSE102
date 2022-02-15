e939e8bbb2e65c7605379e1196026824cc5c16e6
ad1thya_r <adithya.ravi@hotmail.com>
2022-02-15 09:50:17 +0100

    td1 containers changes

--- TD2/generators.py
+++ TD2/generators.py
@@ -8,3 +8,56 @@ def fibs():
     while True:
         yield r0
         r0, r1 = r1, r0 + r1
+g=fibs()
+print([next(g) for _ in range(10)])
+
+def prefix_sums(k):
+    '''generator to sum all integers from k to n'''
+    k0, k1 = k, 2*k+1
+    while True:
+        yield k0
+        k0, k1 = k1, 2*k1+1
+
+g = prefix_sums(10)
+print([next(g) for _ in range(10)])
+
+'''import random
+def interleave(g1,g2):
+    a=0
+    k0, k1= random.choice(next(g1)), 1
+    while True:
+        yield k0
+        k0, k1 = k1, random.choice(next(g1),next(g2))
+g = interleave(prefix_sums(10), fibs())
+print([next(g) for _ in range(10)])'''
+lista=[]
+iteraciones=[0]
+def choose(l, k):
+    if k == len(l):
+        if l not in lista:
+            lista.append(l)
+        return
+    for i in l:
+        aux = l[:]
+        aux.remove(i)
+        result = choose(aux, k)
+        iteraciones[0] += 1
+        if not result in lista and result:
+            lista.append(result)
+    return lista
+
+
+def choose_gen(l: object, k: object) -> object:
+    if k == len(l):
+        if l not in lista:
+            yield l
+        return
+    for i in l:
+        aux = l[:]
+        aux.remove(i)
+        result = choose_gen(aux, k)
+        if result:
+            yield from result
+
+
+
