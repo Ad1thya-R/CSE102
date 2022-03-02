f399d1511ed86949414017a35cda54218bfbabaf
Adithya Ravichandran <adithya.ravi@hotmail.com>
2022-03-02 16:33:34 +0100

    TD3 FINITO!!!!!!

--- TD3: Dynamic programming/dpcoin.py
+++ TD3: Dynamic programming/dpcoin.py
@@ -6,9 +6,20 @@ def transacts_num(n):
     coins=[1,3,7,9]
     c=len(coins)
     for i in range(1,n+1):
+        table[i]=sys.maxsize
+
+    for i in range(1,n+1):
         for j in range(c):
             if coins[j]<=i:
                 sub_res=table[i-coins[j]]
                 if (sub_res!= sys.maxsize and sub_res+1<table[i]):
+                    table[i]=sub_res+1
+    if table[n] ==sys.maxsize:
+        return -1
+
+    return table[n]
+
+print([transacts_num(100 + i) for i in range(10)])
+
 
 
